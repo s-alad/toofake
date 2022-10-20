2bd851ce0eb090a0b98da422493799d938af5028
s-alad <n7.saad@gmail.com>
2022-10-20 02:30:01 -0400

    git logo and link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Instant from './components/instant';
 import Login from './views/login/login';
 import Home from './views/home/home';
+import gitlogo from './static/gitlogo.png';
 
 function App() {
 
@@ -15,15 +16,20 @@ function App() {
   return (
     <div className="App">
       <div className='toofake'>
-        TooFake
+          TooFake
       </div>
       {
 
-        (login && !(sessionStorage.getItem('token') != null)) ? 
-        <Login auth={auth}></Login>
-        : 
-        <Home></Home>
+        (login && !(sessionStorage.getItem('token') != null)) ?
+          <Login auth={auth}></Login>
+          :
+          <Home></Home>
       }
+      <div className='git'>
+          <a href='https://github.com/s-alad/toofake' target="_blank">
+            <img src={gitlogo} /> source code
+          </a>
+        </div>
     </div>
   );
 }
