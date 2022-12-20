6d02fb5b6dccc36d7c53de880fc395f82310b6e2
s-alad <n7.saad@gmail.com>
2022-12-20 03:01:59 -0500

    header fixes

--- server/api.py
+++ server/api.py
@@ -12,8 +12,11 @@ api_url="https://mobile.bereal.com/api"
 google_api_key="AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA"
 
 head = {
-    "user-agent": "AlexisBarreyat.BeReal/0.24.0 iPhone/16.0.2 hw/iPhone12_8 (GTMSUF/1)",
+    "x-firebase-client": "apple-platform/ios apple-sdk/19F64 appstore/true deploy/cocoapods device/iPhone9,1 fire-abt/8.15.0 fire-analytics/8.15.0 fire-auth/8.15.0 fire-db/8.15.0 fire-dl/8.15.0 fire-fcm/8.15.0 fire-fiam/8.15.0 fire-fst/8.15.0 fire-fun/8.15.0 fire-install/8.15.0 fire-ios/8.15.0 fire-perf/8.15.0 fire-rc/8.15.0 fire-str/8.15.0 firebase-crashlytics/8.15.0 os-version/14.7.1 xcode/13F100",
+    "user-agent":"FirebaseAuth.iOS/8.15.0 AlexisBarreyat.BeReal/0.22.4 iPhone/14.7.1 hw/iPhone9_1",
     "x-ios-bundle-identifier": "AlexisBarreyat.BeReal",
+    "x-firebase-client-log-type": "0",
+    "x-client-version": "iOS/FirebaseSDK/8.15.0/FirebaseCore-iOS",
 }
 
 @app.route("/")
@@ -22,12 +25,14 @@ def slash():
 
 @app.route("/sendotp/<phone>")
 def send_otp(phone: str):
+    print(phone)
     res = requests.post(
         url="https://www.googleapis.com/identitytoolkit/v3/relyingparty/sendVerificationCode",
         params={"key":google_api_key},
         data={
                 "phoneNumber": phone,
                 "iosReceipt": "AEFDNu9QZBdycrEZ8bM_2-Ei5kn6XNrxHplCLx2HYOoJAWx-uSYzMldf66-gI1vOzqxfuT4uJeMXdreGJP5V1pNen_IKJVED3EdKl0ldUyYJflW5rDVjaQiXpN0Zu2BNc1c",
+                "iosSecret": "KKwuB8YqwuM3ku0z",
             },
         headers=head
     ).json()
