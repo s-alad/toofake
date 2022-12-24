3a46c9706952e1396aa192031979043f3de3af88
s-alad <n7.saad@gmail.com>
2022-12-24 02:20:48 -0500

    -

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { inject } from '@vercel/analytics';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      {inject()}
+      <App />
+    </>
   </React.StrictMode>
 );
