fabecb0c2c7ed6a2c53ae490cb4fd46a0a1c9615
s-alad <n7.saad@gmail.com>
2023-06-17 15:16:22 -0400

    update

---
+++ new/client/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import Axios from 'axios'
+import { Constants } from '@/constants/constants'
+
+export default function Home() {
+
+
+	return (
+		<main className={styles.main}>
+			<h1 className={styles.title}>toofake</h1>
+			
+		</main>
+	)
+}
---
+++ new/client/constants/constants.ts
@@ -0,0 +1,9 @@
+let api_url = "https://mobile.bereal.com/api"
+let google_api_key="AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA" 
+
+export const Constants = {
+    api_url: api_url,
+    google_api_key: google_api_key,
+    alex_user_agent: "BeReal/1.0.1 (AlexisBarreyat.BeReal; build:9513; iOS 16.0.2) 1.0.0/BRApriKit",
+    x_ios_bundle_identifier: "AlexisBarreyat.BeReal"
+}
