ef46406de26a1a17f18abc9fd697fe14923c041b
s-alad <n7.saad@gmail.com>
2023-06-19 16:46:23 -0400

    start navigation

--- new/client/components/navbar/navbar.tsx
+++ new/client/components/navbar/navbar.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import s from './navbar.module.scss'
 
-import Router from 'next/router'
+import { useRouter } from 'next/router'
+import Link from 'next/link';
 
 export default function Navbar() {
+
+    let router = useRouter();
+    
     return (
         <nav className={s.toofake}>
             <div>Toofake</div>
-            <div className={s.myprofile}>
-                
+            <div className={s.navigation}>
+                {
+                    router.pathname == "/feed" ? 
+                    <> 
+                        <div className={s.sep}></div>
+                        <div className={s.feed}>
+                            Feed
+                        </div>
+                        <Link className={s.profile} href={"/me"}>
+                            <img />
+                        </Link>
+                    </> : ''
+                }
             </div>
         </nav>
     )
