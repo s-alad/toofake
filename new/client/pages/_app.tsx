e207de747bbf7d9b3b20754ed8448dea1ba2b01d
s-alad <n7.saad@gmail.com>
2023-06-21 21:41:21 -0400

    vc

--- new/client/pages/_app.tsx
+++ new/client/pages/_app.tsx
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property='og:description' content="A BeReal Viewer: Post custom BeReal's whenever, View friends BeReals without them knowing "/>
 				<link rel="icon" href="/TooFake.png" />
         <Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5"></Script>
+        <Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca"></Script>
 			</Head>
 
       <Component {...pageProps} />
