e47ee6cb2f433b3790463bfc127668f152d9411f
s-alad <n7.saad@gmail.com>
2023-06-21 22:30:07 -0400

    ua

--- new/client/pages/_app.tsx
+++ new/client/pages/_app.tsx
@@ -7,24 +7,24 @@ import Script from 'next/script';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Head>
+	return (
+		<Layout>
+			<Head>
 				<title>TooFake</title>
 				<meta name="description" content="A BeReal Viewer: Post custom BeReal's whenever, View friends BeReals without them knowing " />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta property='og:title' content='TooFake' />
-        <meta property='og:description' content="A BeReal Viewer: Post custom BeReal's whenever, View friends BeReals without them knowing "/>
+				<meta charSet="utf-8" />
+				<meta property='og:title' content='TooFake' />
+				<meta property='og:description' content="A BeReal Viewer: Post custom BeReal's whenever, View friends BeReals without them knowing " />
 				<link rel="icon" href="/TooFake.png" />
-        <Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5"></Script>
-        <Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca"></Script>
+				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5" strategy="lazyOnload"></Script>
+				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca" strategy="lazyOnload"></Script>
 			</Head>
 
-      <Component {...pageProps} />
+			<Component {...pageProps} />
 
-      <GoogleAnalytics trackPageViews gaMeasurementId='G-0QYBTQCRXD' />
-      <Analytics />
-    </Layout>
-  )
+			<GoogleAnalytics trackPageViews gaMeasurementId='G-0QYBTQCRXD' />
+			<Analytics />
+		</Layout>
+	)
 }
--- new/client/pages/_document.tsx
+++ new/client/pages/_document.tsx
@@ -1,14 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5" strategy="lazyOnload"></Script>
+				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca" strategy="lazyOnload"></Script>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
