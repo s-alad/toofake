9e2082cf985107ac8c381fa6fba40f654d872622
s-alad <n7.saad@gmail.com>
2023-06-23 19:59:10 -0400

    refactor

--- new/client/pages/_app.tsx
+++ new/client/pages/_app.tsx
@@ -18,7 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta charSet="utf-8" />
 				<meta property='og:title' content='TooFake' />
-				<meta property='og:description' content="A BeReal Viewer: Post custom BeReal's whenever, View friends BeReals without them knowing " />
+				<meta property='og:description' 
+					content="A BeReal Viewer: Post custom BeReal's whenever, View friends BeReals without them knowing. Befake. " 
+				/>
 				<link rel="icon" href="/TooFake.png" />
 				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5" strategy="lazyOnload"></Script>
 				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca" strategy="lazyOnload"></Script>
