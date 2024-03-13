6258fab2da5e898b597a85b3f339d8afbfd1cd37
s-alad <n7.saad@gmail.com>
2024-03-13 00:09:45 -0400

    fix umami aly

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -6,8 +6,8 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5" strategy="lazyOnload"></Script>
-				<Script async src="https://toofake-analytics-umami.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca" strategy="lazyOnload"></Script>
+				<Script async src="https://toofake-analytics.up.railway.app/script.js" data-website-id="849b3e73-c171-4ee0-bb3c-fd1aba9fe2d5" strategy="lazyOnload"></Script>
+				<Script async src="https://toofake-analytics.up.railway.app/script.js" data-website-id="fb480e66-7c55-423b-9543-753b669fbfca" strategy="lazyOnload"></Script>
 			</Head>
 			<body>
 				<Main />
