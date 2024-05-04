afdc3c40705b6cac3f1e29399d5e3d7bf6610dbe
s-alad <n7.saad@gmail.com>
2024-05-04 00:59:34 -0400

    add sst, remove old files

--- client/pages/api/middleware.ts
+++
@@ -1,9 +0,0 @@
-// tryig to remove x-vercel-id header from response
-
-import { NextResponse, NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-    const response = NextResponse.next();
-    response.headers.delete('x-vercel-id');
-    return response;
-}
